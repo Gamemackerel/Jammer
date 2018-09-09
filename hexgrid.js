a43480323fedd2c1bdee40bf74edfb46634689c3
Abe Miller <abraham.r.m@gmail.com>
2018-09-09 13:50:44 -0700

    Start controller

--- hexgrid.js
+++ hexgrid.js
@@ -1,73 +1,90 @@
+const OUTOFBOUNDS = -1;
+
 function HexGrid(columns, rows) { //object definition
-	this.model = []
+  this.model = []
 
-	for (var i = 0; i < columns; i++) {
-		this.model[i] = []
-		for (var j = 0; j < rows; j++) {
-			this.model[i][j] = 0;
-		}
-	}
+  for (var i = 0; i < columns; i++) {
+    this.model[i] = []
+    for (var j = 0; j < rows; j++) {
+      this.model[i][j] = 0;
+    }
+  }
 
-	this.getNeighborState = function(column, row, neighborIndex) {
-		var c, r = getNeighborCoords(column, row, neighborIndex);
-		return (this.model[c][r]);
-	}
+  this.getNeighborState = function(column, row, neighborIndex) {
+    var c, r = getNeighborCoords(column, row, neighborIndex);
+    return (this.model[c][r]);
+  }
 
-	this.getNeighborHood = function(column, row) {
-		var result = []
-		for (var i = 0; i < 12; i++) {
-			result.push(this.getNeighborState(column, row, i));
-		}
-		return result;
-	}
+  this.getNeighborHood = function(column, row) {
+    var result = []
+    for (var i = 0; i < 12; i++) {
+      result.push(this.getNeighborState(column, row, i));
+    }
+    return result;
+  }
 
-	this.getState = function(column, row){
-		return this.model[column][row];
-	}
+  this.getState = function(column, row){
+    if (!this.isInBounds(column, row)) {
+      return OUTOFBOUNDS;
+    } else {
+      return this.model[column][row];
+    }
+  }
+
+  this.setState = function(column, row, newState) {
+    if (this.isInBounds(column, row)) {
+      this.model[column][row] = newState;
+    } 
+  }
+
+  this.isInBounds = function(column, row) {
+    return (((column >= 0) && (column < this.model.length)) 
+        && ((row >= 0) && (row < this.model[0].length)))
+  }
 }
 
 
 
 // 12 total neighbors, neighbor directly above is 0 
-// and then the indices are enumerated around clockwise
+// and then the indices are enumerated clockwise
 function getNeighborCoords(column, row, neighborIndex) {
-	switch(neighborIndex) {
-		case 0:
-			return [column, row + 1]
-		break;
-		case 1:
-			return [column + 1, row + 2]
-		break;
-		case 2:
-			return [column + 1, row + 1]			
-		break;
-		case 3:
-			return [column + 2, row]
-		break;
-		case 4:
-			return [column + 1, row]
-		break;
-		case 5:
-			return [column + 1, row - 1]
-		break;
-		case 6:
-			return [column, row - 1]
-		break;
-		case 7:
-			return [column - 1, row - 1]
-		break;
-		case 8:
-			return [column - 1, row]
-		break;
-		case 9:
-			return [column - 2, row]
-		break;
-		case 10:
-			return [column - 1, row + 1]
-		break;
-		case 11:
-			return [column - 1, row + 2]
-		break;
-	}
+  switch(neighborIndex) {
+    case 0:
+      return [column, row + 1]
+    break;
+    case 1:
+      return [column + 1, row + 2]
+    break;
+    case 2:
+      return [column + 1, row + 1]      
+    break;
+    case 3:
+      return [column + 2, row]
+    break;
+    case 4:
+      return [column + 1, row]
+    break;
+    case 5:
+      return [column + 1, row - 1]
+    break;
+    case 6:
+      return [column, row - 1]
+    break;
+    case 7:
+      return [column - 1, row - 1]
+    break;
+    case 8:
+      return [column - 1, row]
+    break;
+    case 9:
+      return [column - 2, row]
+    break;
+    case 10:
+      return [column - 1, row + 1]
+    break;
+    case 11:
+      return [column - 1, row + 2]
+    break;
+  }
 }
 
--- sketch.js
+++ sketch.js
@@ -1,50 +1,89 @@
-var WIDTH = 640;
-var HEIGHT = 480;
-var RADIUS = 40;
-var COLORMAP = {0: 255, 1: 0};
+const WIDTH = 1920;
+const HEIGHT = 1080;
+const RADIUS = 100;
+const COLORMAP = {0: 255, 1: 51};
+
 var grid;
+var pause = true;
 
 
 function setup() {
-  	createCanvas(WIDTH,HEIGHT);
-  	grid = new HexGrid(10, 5);
-  	displayHexGrid(grid, 40); // WHY CAN I NOT USE CLASS CONSTANTS
+    createCanvas(WIDTH,HEIGHT);
+    grid = new HexGrid(10, 5);
 }
 
 function draw() {
-	// grid.step();
-	// displayHexGrid(grid, RADIUS);
+  if (pause) {
+    // open controls for changing seed
+  } else {
+    // grid.step();
+    // displayHexGrid(grid, RADIUS);  
+  }
+  displayHexGrid(grid, RADIUS);
 }
 
 function displayHexGrid(grid, radius) {
-	for (var column = 0; column < grid.model.length; column++) {
-		for (var row = 0; row < grid.model[row].length; row++) {
-			var coords = getXY(column, row, radius);
-			var color = COLORMAP[grid.getState(column, row)];
-			drawHexagon(coords[0], coords[1], radius, color);
-		}
-	}
+  for (var column = 0; column < grid.model.length; column++) {
+    for (var row = 0; row < grid.model[row].length; row++) {
+      var coords = getXY(column, row, radius);
+      var color = COLORMAP[grid.getState(column, row)];
+      drawHexagon(coords[0], coords[1], radius, color);
+    }
+  }
 }
 
 function getXY(column, row, radius) {
-	var inner_radius = (sqrt(3) / 2) * radius;
-	var x = (column * radius * 3/2) + radius;
-	var y = (row * inner_radius * 2) + inner_radius;
-	if (column % 2) { //stagger hexagons on odd rows
-		y += inner_radius;
-	}
-	var result = [x, y];
-	return result;
+  var inner_radius = innerHexRadius(radius);
+  var x = (column * radius * 3/2) + radius;
+  var y = (row * inner_radius * 2) + inner_radius;
+  if (column % 2) { //stagger hexagons on odd rows
+    y += inner_radius;
+  }
+  return [x, y];
+}
+
+function getCR(x, y, radius) {
+  var inner_radius = innerHexRadius(radius);
+  var column = Math.round((2/3)*(x/radius - 1))
+  if (column % 2) { //stagger hexagons on odd rows
+    y -= inner_radius;
+  }
+  var row = Math.round((y - inner_radius) / (inner_radius * 2))
+  var result = [column, row];
+  return result;
+}
+
+function innerHexRadius(radius) {
+  return (sqrt(3) / 2) * radius;
 }
 
 function drawHexagon(x, y, radius, color) {
-	console.log("drawing hexagon at " + x + "," + y);
-	var angle = TWO_PI / 6;
-	beginShape();
-	for (var a = 0; a < TWO_PI; a += angle) {
-	var sx = x + cos(a) * radius;
-	var sy = y + sin(a) * radius;
-	vertex(sx, sy);
-	}
-	endShape(CLOSE);	
+  fill(color);
+  var angle = TWO_PI / 6;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);  
+  noFill();
+}
+
+function mousePressed() {
+  if (pause) {
+    var gridIndex = getCR(mouseX, mouseY, RADIUS);
+    if (grid.isInBounds(gridIndex[0], gridIndex[1], RADIUS)) {
+      var newState = grid.getState(gridIndex[0], gridIndex[1]) ? 0 : 1;
+      grid.setState(gridIndex[0], gridIndex[1], newState);
+    }
+  }
+  return false;
+}
+
+function keyPressed() {
+  if(keyCode == 32) {
+      pause = !pause;
+  }
+  return 0;
 }
